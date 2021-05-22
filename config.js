855c76e1d4e43cae9ede07d0fa953d599061d535
Mikołaj Gramowski <emeraldbob020@gmail.com>
2021-05-22 20:31:36 +0200

    final

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:pjrt6ulocdrHHlOn@cluster0.xkaib.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const News = require('../model/news')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('news', { title: 'News' });
+  const search = req.query.search || ''
+
+  const findNews = News.find({title: new RegExp(search.trim(), 'i')}).sort({created: -1})
+  data.exec((err, data) => {
+  res.render('news', {title: 'News', data, search})
+  })
 });
 
 module.exports = router;
