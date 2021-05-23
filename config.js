9baa98e26298b5c33d043512f77b745576e2a805
GramosTV <50053700+GramosTV@users.noreply.github.com>
2021-05-23 12:19:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: 'mongodb+srv://admin:pjrt6ulocdrHHlOn@cluster0.xkaib.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    db: 'mongodb+srv://admin:passwordn@cluster0.xkaib.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000
 }
